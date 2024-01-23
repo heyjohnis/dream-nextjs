b34c086da7af17be6a479f161c1853f9a5629e02
heyjohnis <juni0227@nate.com>
2024-01-23 15:34:51 +0900

    4.11 Not Found 페이지

---
+++ app/not-found.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function notFoundPage() {
+  return <div>not-found</div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 export default function Home() {
+  // notFound();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 }
